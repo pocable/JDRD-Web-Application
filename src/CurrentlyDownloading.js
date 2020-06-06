e8442bae5058e9f25904be9fbb3ad85e9f9e4698
Thomas <thomas354@shaw.ca>
2020-06-05 22:17:49 -0600

    Aded currently downloading class

---
+++ src/CurrentlyDownloading.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Card, ListGroup} from 'react-bootstrap';
+
+
+export default class CurrentlyDownloading extends React.Component{
+
+      
+    state = {'curDownload': []}
+
+    constructor(props){
+        super(props);
+        this.getCurrentDownloads = this.getCurrentDownloads.bind(this);
+    }
+
+    componentDidMount(){
+        this.getCurrentDownloads();
+    }
+
+    getCurrentDownloads(){
+        fetch('http://192.168.0.38:4248/api/v1/content/all', {
+            method: 'get',
+            headers: new Headers({
+                'Authorization': 'ccbce173ee8b1e0ad838dc4198ee15ff9f0b0e1cfe07f964201cab0a11b4e9e8',
+                'Content-Type': 'application/json'
+            })
+            }).then(response => {
+                return response.json();
+            }).then(data => {
+                var items = []
+                for(var x in data){
+                    items.push((<ListGroup.Item>{data[x]['title']} <br></br> {data[x]['path']} - {x}</ListGroup.Item>))
+                }
+                this.setState({'curDownload': items});
+            }
+        );
+    }
+
+    render(){
+        return (
+            <div className='CurrentDownloads'>
+                <Card>
+                    <Card.Header>
+                        Current Downloads
+                    </Card.Header>
+                    <ListGroup variant="flush">
+                        {this.state.curDownload}
+                    </ListGroup>
+                </Card>
+            </div>
+        );
+    }
+}
